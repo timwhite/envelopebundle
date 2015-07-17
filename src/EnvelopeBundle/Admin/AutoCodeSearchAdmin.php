eed6be485bf609dbe191286fd5ab7b2a83c01ed9
Tim White <tim@whiteitsolutions.com.au>
2015-07-17 16:07:53 +1000

    Added auto code search admin

---
+++ src/EnvelopeBundle/Admin/AutoCodeSearchAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EnvelopeBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class AutoCodeSearchAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('search', 'text', array('label' => 'Search Text'))
+            ->add('budgetAccount', 'entity', array('class' => 'EnvelopeBundle\Entity\BudgetAccount'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('search')
+            ->add('budgetAccount')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('search')
+            ->add('budgetAccount')
+        ;
+    }
+}
