ca7184e62befd284d61fbae340d07906dd8892f7
Tim White <timwhite88@gmail.com>
2015-07-22 07:01:21 +1000

    Bring in a menu system

---
+++ src/EnvelopeBundle/EventListener/MyMenuItemListListener.php
@@ -0,0 +1,56 @@
+<?php
+namespace EnvelopeBundle\EventListener;
+
+use Avanzu\AdminThemeBundle\Model\MenuItemModel;
+use Avanzu\AdminThemeBundle\Event\SidebarMenuEvent;
+use Symfony\Component\HttpFoundation\Request;
+
+class MyMenuItemListListener {
+
+    // ...
+
+    public function onSetupMenu(SidebarMenuEvent $event) {
+
+        $request = $event->getRequest();
+
+        foreach ($this->getMenu($request) as $item) {
+            $event->addItem($item);
+        }
+
+    }
+
+    protected function getMenu(Request $request) {
+        // retrieve your menuItem models/entities here
+        $items = [
+            'envelope_budget_templates' => "Budget Templates",
+            'envelope_budgets' => "Budgets",
+            'envelope_transactions' => "Bank Transactions",
+            'envelope_budget_apply_template' => "Apply Budget Template",
+        ];
+        $menuItems = array();
+        foreach($items as $key => $label)
+        {
+            $menuItems[] = new MenuItemModel($key, $label, $key);
+        }
+
+
+        return $this->activateByRoute($request->get('_route'), $menuItems);
+    }
+
+    protected function activateByRoute($route, $items) {
+
+        foreach($items as $item) {
+            if($item->hasChildren()) {
+                $this->activateByRoute($route, $item->getChildren());
+            }
+            else {
+                if($item->getRoute() == $route) {
+                    $item->setIsActive(true);
+                }
+            }
+        }
+
+        return $items;
+    }
+
+}
