5b0cdbdfb7200ac4e33edacbb1355d727e15ee99
Tim White <tim@whiteitsolutions.com.au>
2021-07-24 22:12:00 +0800

    Initial flex install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -47,7 +47,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), [ 'dev', 'test' ] )) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
