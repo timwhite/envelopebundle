62d103cb7eaa5fc9d3b00d137950dc68ce5c02c8
Tim White <tim@whiteitsolutions.com.au>
2022-01-15 21:08:06 +0800

    Fix migrations

--- migrations/Version20160302211131.php
+++ migrations/Version20160302211131.php
@@ -2,7 +2,7 @@
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -13,7 +13,7 @@ class Version20160302211131 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE account ADD budget_transfer TINYINT(1) NOT NULL');
@@ -24,7 +24,7 @@ class Version20160302211131 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE account DROP budget_transfer');
