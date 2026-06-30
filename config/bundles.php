3cf73e90c4a6f7e20be8a06cd66e537ebd3ba4bb
Tim White <tim@whiteitsolutions.com.au>
2026-06-30 16:14:00 +1000

    Remove API platform

--- config/bundles.php
+++ config/bundles.php
@@ -14,7 +14,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
